4319f7116712a42181260582de5d2fff97e99085
Svetlin Stanchev <ilms88@gmail.com>
2019-01-19 16:35:51 +0200

    update: movies object

--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Movie } from './store/movie.model';
-import { map } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
 
 @Injectable()
@@ -9,7 +7,20 @@ export class MoviesService {
   constructor() {}
 
   getMovies(): Observable<Movie[]> {
-    const movies: Movie[] = [{ id: '1' }, { id: '2' }];
+    const movies: Movie[] = [
+      {
+        id: '1',
+        name: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+        posterUrl:
+          'https://m.media-amazon.com/images/M/MV5BZWI3ZTMxNjctMjdlNS00NmUwLWFiM2YtZDUyY2I3N2MxYTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg'
+      },
+      {
+        id: '2',
+        name: 'Star Wars: Episode VI - Return of the Jedi',
+        posterUrl:
+          'https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY999_CR0,0,644,999_AL_.jpg'
+      }
+    ];
     return of(movies);
   }
 }
--- src/app/movies/store/movie.model.ts
+++ src/app/movies/store/movie.model.ts
@@ -1,3 +1,5 @@
 export interface Movie {
   id: string;
+  name: string;
+  posterUrl: string;
 }
